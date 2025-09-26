bb35338f1b09ea1ca5e86f35d1985a4533a4f971
welaskez <nazar.nikitenko.03@mail.ru>
2025-09-26 13:08:13 +0500

    feat: use TonClient4

--- src/hooks/useTonClient.ts
+++ src/hooks/useTonClient.ts
@@ -1,12 +1,11 @@
-import { getHttpEndpoint } from "@orbs-network/ton-access";
-import { TonClient } from "@ton/ton";
+import { TonClient4 } from "@ton/ton";
 import useAsyncInitialize from "./useAsyncInitialize";
 
 export default function useTonClient() {
     return useAsyncInitialize(
         async () =>
-            new TonClient({
-                endpoint: await getHttpEndpoint(),
+            new TonClient4({
+                endpoint: 'https://mainnet-v4.tonhubapi.com/',
             }),
     )
 }
