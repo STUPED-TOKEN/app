ef827fa32445213197ab1712ea53e3aa9ff02e62
welaskez <nazar.nikitenko.03@mail.ru>
2025-09-29 23:08:52 +0500

    feat: handle if testnet

--- src/hooks/useTonClient.ts
+++ src/hooks/useTonClient.ts
@@ -1,11 +1,12 @@
 import { TonClient4 } from "@ton/ton";
 import useAsyncInitialize from "./useAsyncInitialize";
+import { IS_TESTNET } from "../consts";
 
 export default function useTonClient() {
     return useAsyncInitialize(
         async () =>
             new TonClient4({
-                endpoint: 'https://mainnet-v4.tonhubapi.com/',
+                endpoint: IS_TESTNET ? 'https://testnet-v4.tonhubapi.com/' : 'https://mainnet-v4.tonhubapi.com/',
             }),
     )
 }
