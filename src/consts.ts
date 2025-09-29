efc586de5b2f8b9638d3d1e1807a785903fe8f9d
welaskez <nazar.nikitenko.03@mail.ru>
2025-09-29 23:08:27 +0500

    feat: added consts

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+import { Address, toNano } from "@ton/core";
+
+export const COLLECTION_ADDRESS = Address.parse('kQDUWS-IqWxydhfNZG_nTTkoMBi-dj_gWxsifWIDbrjezbB0');
+
+export const IS_TESTNET = true;
+
+export const FEE = toNano("0.125")
